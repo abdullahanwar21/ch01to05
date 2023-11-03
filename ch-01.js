7f3551b7dc05782f0a67b5aef4faa0dbb86ce9ec
abdullahanwar21 <abdullahmeer396@gmail.com>
2023-11-02 23:30:12 -0700

    i have finished my assignment ch 1 to 5

---
+++ ch-01.js
@@ -0,0 +1,8 @@
+// Q-1
+alert("Welcome To Our Website");
+// Q-2
+alert("Error ! Please Enter A Valid Password");
+// Q-3
+alert(`Welcome To JS Land \n Happy Coding!`);
+// Q-4
+console.log(alert(`Hello.... I Can Run JS Through My Web browser Console`));
---
+++ ch-03.js
@@ -0,0 +1,15 @@
+
+        let age = 21;
+        // alert("I am " + age + "Years Old");
+        let visits = 14;
+        // alert("You Have Visited This Site  " + visits + " Times");
+        let birthYear = "My Birth Year is 2002";
+        let convertBirth = typeof Number(birthYear)
+        let decBirthYear = "Data TYpe of My Variable is Number";
+        document.write(birthYear, "</br>");
+        document.write(decBirthYear, "</br>");
+        let visitors = "Meer Abdullah";
+        let productTitle = "5T-Shirt(s)";
+        let storeName = "Chase Up";
+
+        document.write(`<b>${visitors}</b> `, " Ordered ", `<b>${productTitle}</b>`, " On ", `<b>${storeName}</b>`);
